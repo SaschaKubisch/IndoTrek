48fc35c0a6ec945c6f4aae74407e8367fe71d481
Sascha <sascha.fh.kubisch@gmail.com>
2023-05-22 19:12:45 +0200

    Added LoginForm component

---
+++ client/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic
+  };
+
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2 className="login-title">Login</h2>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" className="submit-button">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
